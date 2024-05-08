7bfac3c57afe7dba6260e0b62baa535fa72c63c4
Elnard09 <edmvallejo2020@plm.edu.ph>
2024-05-08 15:46:13 +0800

    export to excel
    okay na ang excel

--- Admin Interface/export_excel.php
+++ Admin Interface/export_excel.php
@@ -6,8 +6,12 @@ include("../sql/config.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $selectedIds = json_decode($_POST['selected']);
 
-    // Select leave applications with the selected ids
-    $sql = "SELECT l.*, UCASE(CONCAT(u.lastname, ', ', u.firstname)) AS full_name
+    // Select leave applications with the selected ids and user details
+    $sql = "SELECT l.*, 
+                   CONCAT(u.lastname) AS last_name, 
+                   CONCAT(u.firstname) AS first_name, 
+                   u.contactnumber, 
+                   u.department 
             FROM leave_applications AS l 
             INNER JOIN users AS u ON l.user_id = u.user_id
             WHERE l.id IN (" . implode(",", $selectedIds) . ")
@@ -16,9 +20,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($result->num_rows > 0) {
         // Define CSV content
-        $csvData = "Full Name,Type of Leave,Date Filed,Date Requested,Leave Until\n";
+        $csvData = "Last Name,First Name,Date Filed,Department,Contact Number,Type of Leave,Date Requested,Leave Until,Working days covered,Reason\n";
         while($row_data = $result->fetch_assoc()) {
-            $csvData .= '"' . $row_data['full_name'] . '","' . $row_data['leave_type'] . '","' . $row_data['date_filed'] . '","' . $row_data['from_date'] . '","' . $row_data['to_date'] . "\"\n";
+            $csvData .= '"' . $row_data['last_name'] . '","' . $row_data['first_name'] . '","' . $row_data['date_filed'] . '","' . $row_data['department'] . '","' . $row_data['contactnumber'] . '","' . $row_data['leave_type'] . '","' . $row_data['from_date'] . '","' . $row_data['to_date'] . '","' . $row_data['working_days_covered'] . '","' . $row_data['reason'] . "\"\n";
         }
 
         // Set headers to force download
