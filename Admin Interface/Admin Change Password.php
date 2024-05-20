8a8450e1a8c7e4e0941117f55ea76b80b2a27dbd
Jasme24 <jbtantoy0224@gmail.com>
2024-05-20 10:52:12 +0800

    Removed unnecessary things

--- Admin Interface/Admin Change Password.php
+++ Admin Interface/Admin Change Password.php
@@ -81,7 +81,6 @@
     <!-- Sidebar -->
   <div class="sidebar" id="sidebar">
     <a href="Admin Home.php" id="active"><i class="fa fa-home"></i> Home</a>
-    <a href="Admin Dashboard.php" class="home-sidebar"><i class="fa fa-pie-chart"></i> Dashboard</a>
     <span class="leave-label">LEAVE REPORTS</span>
     <a href="Pending Leaves.php"><i class="fa fa-file-text-o"></i> Pending Leaves</a>
     <a href="Approved Leaves.php"><i class="fa fa-file-word-o"></i> Approved Leaves</a>
